127c58f3caf8c460feb077a5de8ff993cbcb3efc
Mark Hammond <m.w.hammond@live.co.uk>
2013-04-28 18:42:37 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Futureproject::Application.config.secret_token = '9fa411262616da8ef425d48d3fbd278d1cf4ba768f78fd1aac92c772ac6e7f28b82c3ef6652598e994f3cd371f43efc6888b9a1b4b6388adaf2868dcc559657e'
